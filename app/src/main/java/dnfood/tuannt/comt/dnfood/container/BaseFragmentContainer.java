329462116c47572582dd6b45919aa3a29ff0c5db
TuanNT <tuannt.itedu@gmail.com>
2015-07-28 00:34:14 +0700

    create project

---
+++ app/src/main/java/dnfood/tuannt/comt/dnfood/container/BaseFragmentContainer.java
@@ -0,0 +1,12 @@
+package dnfood.tuannt.comt.dnfood.container;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Copyright Tuannt
+ * Created by TuanNT on 7/27/2015.
+ */
+public abstract class BaseFragmentContainer extends Fragment{
+
+
+}
---
+++ app/src/main/java/dnfood/tuannt/comt/dnfood/fragments/BaseFragment.java
@@ -0,0 +1,10 @@
+package dnfood.tuannt.comt.dnfood.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Copyright Tuannt
+ * Created by TuanNT on 7/27/2015.
+ */
+public class BaseFragment  extends Fragment{
+}
